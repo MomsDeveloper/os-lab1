dfb812b34fca3159f4412afed90cb67675bc626d
Георгий Хабнер <georgiy.khabner@mail.ru>
2024-11-01 04:13:54 +0300

    impl brench1

--- generate_numbers.py
+++ generate_numbers.py
@@ -3,22 +3,27 @@ from random import randint
 
 
 filename = Path('os-lab1/data/myfile.bin')
+# file = Path('os-lab1/data/f2.bin')
 
 # max storage in MB
-max_storage = 256
+max_storage = 16
 # symbol storage in bytes
 symbol_storage = 4
 
 
 with open(filename, 'wb') as f:
     for _ in range(max_storage * 1024 * 1024 // symbol_storage):
-        random_numbers = randint(0, 2**32 - 1).to_bytes(symbol_storage, 'little')
+        random_numbers = randint(0, 2**16 - 1).to_bytes(symbol_storage, 'little')
         f.write(random_numbers)
-
 # print(f'File {filename} was created')
 
-with open(filename, 'rb') as f:
-    print(int.from_bytes(f.read(symbol_storage), 'little'))
-    print(int.from_bytes(f.read(symbol_storage), 'little'))
-#     for _ in range(max_storage * 1024 * 1024 // symbol_storage):
-#         print(int.from_bytes(f.read(symbol_storage), 'big'))
+# with open(filename, 'rb') as f:
+#     # print(int.from_bytes(f.read(symbol_storage), 'little'))
+#     # print(int.from_bytes(f.read(symbol_storage), 'little'))
+#     for _ in range(max_storage // symbol_storage):
+#         print(int.from_bytes(f.read(symbol_storage), 'little'))
+
+# print(f'File {filename} was read')
+# with open(file, "rb") as f:
+#     for _ in range(max_storage // symbol_storage):
+#         print(int.from_bytes(f.read(symbol_storage), 'little'))
--- src/bench1.c
+++ src/bench1.c
@@ -1,77 +1,124 @@
 #include "bench1.h"
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <string.h>
 
-#define BLOCK_SIZE 2 
+void merge(FILE *f, FILE *f1, FILE *f2, int k, int a1, int a2)
+{
+    int i, j;
 
-void swap (int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
+    if (!feof(f1))
+        fread(&a1, sizeof(int), 1, f1);
+    if (!feof(f2))
+        fread(&a2, sizeof(int), 1, f2);
 
-
-
-void sort_array(FILE *filestream) {
-
-    int *elements;
-    elements = (int *)malloc(2 * sizeof(int));
-
-    if (elements == NULL) {
-        fprintf(stderr, "\nError allocating memory\n");
-        exit(1);
-    }
-
-    int values_count;
-    fseek(filestream, 0, SEEK_END);
-    values_count = ftell(filestream) / 4;
-    rewind(filestream);
-
-    if (values_count < 2) {
-        fprintf(stderr, "\nNot enough values in file\n");
-        exit(1);
-    }
-
-    for (int i = 0; i < values_count - 1; i++) {
-        for (int j = 0; j < values_count - i - 1; j++) {
-            fseek(filestream, j * sizeof(int), SEEK_SET);
-            fread(elements, sizeof(int), 2, filestream);
-            if (elements[0] > elements[1]) {
-                swap(&elements[0], &elements[1]);
-
-                fseek(filestream, j * sizeof(int), SEEK_SET);
-                fwrite(elements, sizeof(int), 2, filestream);
+    while (!feof(f1) && !feof(f2))
+    {
+        i = 0;
+        j = 0;
+        while (i < k && j < k && !feof(f1) && !feof(f2))
+        {
+            if (a1 < a2)
+            {
+                fwrite(&a1, sizeof(int), 1, f);
+                fread(&a1, sizeof(int), 1, f1);
+                i++;
             }
-
+            else
+            {
+                fwrite(&a2, sizeof(int), 1, f);
+                fread(&a2, sizeof(int), 1, f2);
+                j++;
+            }
         }
-
+        while (i < k && !feof(f1))
+        {
+            fwrite(&a1, sizeof(int), 1, f);
+            fread(&a1, sizeof(int), 1, f1);
+            i++;
+        }
+        while (j < k && !feof(f2))
+        {
+            fwrite(&a2, sizeof(int), 1, f);
+            fread(&a2, sizeof(int), 1, f2);
+            j++;
+        }
     }
-
-    free(elements);
-
-    for (int i = 0; i < values_count; i++) {
-        fseek(filestream, i * sizeof(int), SEEK_SET);
-        fread(elements, sizeof(int), 1, filestream);
-        printf("%d\n", elements[0]);
+    while (!feof(f1))
+    {
+        fwrite(&a1, sizeof(int), 1, f);
+        fread(&a1, sizeof(int), 1, f1);
+    }
+    while (!feof(f2))
+    {
+        fwrite(&a2, sizeof(int), 1, f);
+        fread(&a2, sizeof(int), 1, f2);
     }
 }
 
+void simple_merging_sort(char *name)
+{
+    int a1, a2, kol, k;
+    kol = 0;
 
-
-// void bubbleSort(
-
-int main(){
-    FILE *filestream;
-    filestream = fopen("./data/myfile.bin", "rb+");
-    if (filestream == NULL) {
-        fprintf(stderr, "\nError opening file\n");
-        exit(1);
+    FILE *f, *f1, *f2;
+    if ((f = fopen("data/myfile.bin", "rb")) == NULL)
+    {
+        printf("Such file doesn't exist");
+    }
+    else
+    {
+        fseek(f, 0, SEEK_END);
+        kol = ftell(f) / 4;
+        rewind(f);
+        printf("%d", kol);
     }
 
-    sort_array(filestream);
+    k = 1;
+    while (k < kol)
+    {
+        f = fopen(name, "rb");
+        f1 = fopen("data/f1.bin", "wb");
+        f2 = fopen("data/f2.bin", "wb");
+        if (!feof(f))
+        {
+            fread(&a1, sizeof(int), 1, f);
+        }
+        while (!feof(f))
+        {
+            for (int i = 0; i < k && !feof(f); i++)
+            {
+                fwrite(&a1, sizeof(int), 1, f1);
+                fread(&a1, sizeof(int), 1, f);
+            }
+            for (int j = 0; j < k && !feof(f); j++)
+            {
+                fwrite(&a1, sizeof(int), 1, f2);
+                fread(&a1, sizeof(int), 1, f);
+            }
+        }
+        fclose(f1);
+        fclose(f2);
+        fclose(f);
 
-    fclose(filestream);
+        f = fopen(name, "wb");
+        f1 = fopen("data/f1.bin", "rb");
+        f2 = fopen("data/f2.bin", "rb");
+        
+        merge(f, f1, f2, k, a1, a2);
+        
+        fclose(f2);
+        fclose(f1);
+        fclose(f);
+        
+        k = k*2;
+    }
+}
+
+int main()
+{
+    char *filename = "./data/myfile.bin";
+    simple_merging_sort(filename);
     return 0;
 }
